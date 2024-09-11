1e6d2d30fc96c8f5e21cfe521d7a20a56ad0cb07
thqi3541 <jifeng.qiu@mail.utoronto.ca>
2024-09-11 18:05:00 -0400

    Add Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,13 +1,29 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
         int count = 0;
 
-        // loop through numbers below 1000
-        for (int i = 1; i < 1000; i++){
-            if (i % 3 == 0 || i % 5 == 0){
-                count++;
+        if (a == b) {
+            for (int i = 1; i < n; i++) {
+                if (i % a == 0 || i % b == 0) {
+                    count += i;
+                }
+            }
+        } else {
+            for (int i = 1; i < n; i++) {
+                if (i % a == 0 || i % b == 0) {
+                    count += i;
+                }
             }
         }
-        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args){
+        int n = 1000;
+        int a = 3;
+        int b = 2;
+
+        int result = main(n,a,b);
+        System.out.println(result);
     }
 }
